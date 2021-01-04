d0151c5fcaf91cd02957ebb9aec7c0fdd878f009
Caue <nncl@live.com>
2021-01-04 09:49:53 -0300

    feat(pages): Adding logo on SignIn Page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Image } from 'react-native';
 import { Container } from './styles';
 
+import logo from '../../assets/logo.png';
+
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logo} />
+    </Container>
+  );
 };
 
 export default SignIn;
