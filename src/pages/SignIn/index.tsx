bf9a851a5ceb07d135d203e56611b7611e946c0b
Caue <nncl@live.com>
2021-01-04 10:34:00 -0300

    feat(icons): Setting upt icons

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,12 +3,19 @@ import { Image } from 'react-native';
 import { Container, Title } from './styles';
 
 import logo from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logo} />
       <Title>Faça seu login</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => console.log('Pressed')}>Entrar</Button>
     </Container>
   );
 };
